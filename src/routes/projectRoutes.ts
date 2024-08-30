1e071ba510d6d678cd0e1aabdacdcf06f6e0b77d
paranaSorteo <paranasorteo@gmail.com>
2024-08-30 20:51:15 -0300

    validacion Create

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,24 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router();
 
-router.post("/", ProjectController.createProject);
+router.post(
+  "/",
+  body("projectName")
+    .notEmpty()
+    .withMessage("El campo Nombre del Proyecto es Obligatorio"),
+  body("clientName")
+    .notEmpty()
+    .withMessage("El campo Cliente del Proyecto es Obligatorio"),
+  body("description")
+    .notEmpty()
+    .withMessage("El campo Descripción del Proyecto es Obligatorio"),
+  handleInputErrors,
+  ProjectController.createProject
+);
 router.get("/", ProjectController.getAllProjects);
 
 export default router;
