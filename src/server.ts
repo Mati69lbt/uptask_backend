6bcbb9cfe5725c844c60bcefa0950c0dcc5a8ffe
paranaSorteo <paranasorteo@gmail.com>
2024-08-30 14:50:46 -0300

    arquitectura

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
+import projectRoutes from "./routes/projectRoutes";
 
 dotenv.config();
 
-connectDB()
+connectDB();
 
 const app = express();
+
+//Routes
+
+app.use("/api/projects", projectRoutes);
 
 export default app;
