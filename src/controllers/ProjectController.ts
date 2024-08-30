c3bebad759c58a6dcb4c07be79ed685d7db9d4b4
paranaSorteo <paranasorteo@gmail.com>
2024-08-30 15:12:04 -0300

    create

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,6 +1,18 @@
-import type { Request, Response } from "express"
+import type { Request, Response } from "express";
+import Project from "../models/Projects";
 export class ProjectController {
-  static getAllProjects= async(req: Request, res: Response) => {
-    res.send('Todos los Proyectos')
-  }
+  static createProject = async (req: Request, res: Response) => {
+    const project = new Project(req.body);
+
+    try {
+      await project.save();
+      res.status(201).json({ message: "Project created successfully" });
+    } catch (error) {
+      console.error(error);
+      console.log(error);
+    }
+  };
+  static getAllProjects = async (req: Request, res: Response) => {
+    res.send("Todos los Proyectos");
+  };
 }
