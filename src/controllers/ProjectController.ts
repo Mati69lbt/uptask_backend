bfe40d36a2292742973bb8bc34495ae0082dcaec
paranaSorteo <paranasorteo@gmail.com>
2024-09-24 16:16:44 -0300

    act28

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,12 +1,13 @@
 import type { Request, Response } from "express";
 import Project from "../models/Projects";
+
 export class ProjectController {
   static createProject = async (req: Request, res: Response) => {
     const project = new Project(req.body);
 
     try {
       await project.save();
-      res.status(201).json({ message: "Project created successfully" });
+      res.status(201).json({ message: "Project Created Successfully" });
     } catch (error) {
       console.error(error);
       console.log(error);
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/projectRoutes";
+import cors from "cors";
+import { corsConfig } from "./config/cors";
 
 dotenv.config();
 
@@ -9,7 +11,9 @@ connectDB();
 
 const app = express();
 
-app.use(express.json())
+app.use(cors(corsConfig));
+
+app.use(express.json());
 
 //Routes
 
